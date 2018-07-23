dd0856a7b1cc6d2da89906d1cec9e1535c5c979e
mayur.kapadia <lanetteam.mayur35@gmail.com>
2018-07-23 18:45:19 +0530

    Completed the Home Page

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -5,51 +5,47 @@ export default {
   components: {
     headermenu
   },
-  data: () => ({
-    url: 'https://api.blavity.com/v1/articles/tags/cardi-b/4/0',
-    fetchLatestNews: [],
-    articleUrl: 'https://api.blavity.com/v1/articles/10/0',
-    baseImageUrl: 'https://res.cloudinary.com/blavity/image/upload/c_crop,g_center,w_auto,q_auto:best,g_south_east,x_0/',
-    videourl: 'https://api.blavity.com/v1/articles/type/video/2',
-    videoData: [],
-    articleData: [],
-    cardMenu1: [
-      { src: 'https://cdn.vuetifyjs.com/images/cards/desert.jpg',
-        title: 'Kangaroo Valley Safari1',
-        content: 'Located two hours south of Sydney in the <br>Southern Highlands of New South Wales, ...'
-      },
-      { src: 'https://cdn.vuetifyjs.com/images/cards/desert.jpg',
-        title: 'Kangaroo Valley Safari2',
-        content: 'Located two hours south of Sydney in the <br>Southern Highlands of New South Wales, ...'
-      },
-      { src: 'https://cdn.vuetifyjs.com/images/cards/desert.jpg',
-        title: 'Kangaroo Valley Safari3',
-        content: 'Located two hours south of Sydney in the <br>Southern Highlands of New South Wales, ...'
-      }
-    ],
-    cardMenu2: [
-      { src: 'https://cdn.vuetifyjs.com/images/cards/desert.jpg',
-        title: 'Dr. Mareena Robinson Snowden On Being The First Black Woman To Earn Her PhD In Nuclear Science From MIT1',
-        content: 'News } Christine'
-      },
-      { src: 'https://cdn.vuetifyjs.com/images/cards/desert.jpg',
-        title: 'Dr. Mareena Robinson Snowden On Being The First Black Woman To Earn Her PhD In Nuclear Science From MIT2',
-        content: 'News } Christine'
-      },
-      { src: 'https://cdn.vuetifyjs.com/images/cards/desert.jpg',
-        title: 'Dr. Mareena Robinson Snowden On Being The First Black Woman To Earn Her PhD In Nuclear Science From MIT3',
-        content: 'News } Christine'
-      },
-      { src: 'https://cdn.vuetifyjs.com/images/cards/desert.jpg',
-        title: 'Dr. Mareena Robinson Snowden On Being The First Black Woman To Earn Her PhD In Nuclear Science From MIT4',
-        content: 'News } Christine'
-      }
-    ],
-    placeimage: [
-      {src: 'http://via.placeholder.com/850x440', title: '1'},
-      {src: 'http://via.placeholder.com/850x440', title: '2'}
-    ]
-  }),
+  data () {
+    return {
+      url: 'https://api.blavity.com/v1/articles/tags/cardi-b/4/0',
+      fetchLatestNews: [],
+      articleUrl: 'https://api.blavity.com/v1/articles',
+      articleLoadCount: 10,
+      articleTotalCount: 0,
+      communityUrl: 'https://api.blavity.com/v1/articles/categories/community-submitted/3',
+      baseImageUrl: 'https://res.cloudinary.com/blavity/image/upload/c_crop,g_center,w_auto,q_auto:best,g_south_east,x_0/',
+      videourl: 'https://api.blavity.com/v1/articles/type/video/2',
+      videoData: [],
+      articleData: [],
+      communityData: [],
+      cardMenu2: [
+        {
+          src: 'https://cdn.vuetifyjs.com/images/cards/desert.jpg',
+          title: 'Dr. Mareena Robinson Snowden On Being The First Black Woman To Earn Her PhD In Nuclear Science From MIT1',
+          content: 'News } Christine'
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/cards/desert.jpg',
+          title: 'Dr. Mareena Robinson Snowden On Being The First Black Woman To Earn Her PhD In Nuclear Science From MIT2',
+          content: 'News } Christine'
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/cards/desert.jpg',
+          title: 'Dr. Mareena Robinson Snowden On Being The First Black Woman To Earn Her PhD In Nuclear Science From MIT3',
+          content: 'News } Christine'
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/cards/desert.jpg',
+          title: 'Dr. Mareena Robinson Snowden On Being The First Black Woman To Earn Her PhD In Nuclear Science From MIT4',
+          content: 'News } Christine'
+        }
+      ],
+      placeimage: [
+        {src: 'http://via.placeholder.com/850x440', title: '1'},
+        {src: 'http://via.placeholder.com/850x440', title: '2'}
+      ]
+    }
+  },
 
   mounted () {
     axios.get(this.url)
@@ -59,8 +55,7 @@ export default {
       .catch(e => {
         console.log(e)
       })
-
-    axios.get(this.articleUrl)
+    axios.get(this.articleUrl + '/' + this.articleLoadCount + '/' + this.articleTotalCount)
       .then((res) => {
         this.articleData = res.data
       })
@@ -75,5 +70,27 @@ export default {
       .catch(e => {
         console.log(e)
       })
+    axios.get(this.communityUrl)
+      .then((res) => {
+        this.communityData = res.data
+      })
+      .catch(e => {
+        console.log(e)
+      })
+  },
+  methods: {
+    articleLoadMore: function (total) {
+      let _this = this
+      // console.log(this.$data.articleTotalCount)
+
+      axios.get('https://api.blavity.com/v1/articles/' + _this.articleLoadCount + '/' + (_this.articleTotalCount + total))
+        .then((res) => {
+          this.articleData.push(...res.data)
+          this.articleTotalCount += res.data.length
+        })
+        .catch(e => {
+          console.log(e)
+        })
+    }
   }
 }
