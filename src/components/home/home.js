d45ebddeb053f4a2a3dcd5cefc67b79e261f19e9
ldeveloper7 <lanetteam.mayur@gmail.com>
2018-08-02 12:51:14 +0530

    added trending articles section on home page

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -17,6 +17,7 @@ export default {
       videoData: [],
       articleData: [],
       communityData: [],
+      trending_articles: [],
       valid: true,
       name: '',
       email: '',
@@ -35,11 +36,13 @@ export default {
     axios.all([
       axios.get(process.env.LiveAPI + 'articles' + '/' + _this.articleLoadCount + '/' + _this.articleTotalCount).catch(null),
       axios.get(process.env.LiveAPI + 'articles/type/video/2').catch(null),
-      axios.get(process.env.LiveAPI + 'articles/categories/community-submitted/3').catch(null)
-    ]).then(axios.spread(function (res1, res2, res3) {
+      axios.get(process.env.LiveAPI + 'articles/categories/community-submitted/3').catch(null),
+      axios.get(process.env.LiveAPI + 'trending_articles').catch(null)
+    ]).then(axios.spread(function (res1, res2, res3, res4) {
       _this.articleData = res1.data
       _this.videoData = res2.data
       _this.communityData = res3.data
+      _this.trending_articles = res4.data
     })).catch(e => {
       console.log(e)
     })
