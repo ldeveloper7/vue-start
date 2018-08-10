6f0e2fb35ba40235eaf9b7cba4a5fc954ad3f2ac
mayurkapadia35 <lanetteam.mayur35@gmail.com>
2018-08-10 17:39:29 +0530

    Make a footer and put it in all the page.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,29 @@
 'use strict'
+
 export default {
-  components: {},
-  data: () => ({}),
-  watch: {},
-  mounted () {},
-  methods: {}
+  data: () => ({
+    logos: [
+      {icons: 'fab fa-facebook-f', link: 'https://www.facebook.com/Blavity/'},
+      {icons: 'fab fa-instagram', link: 'https://www.instagram.com/blavity/'},
+      {icons: 'fab fa-twitter', link: 'https://twitter.com/blavity'},
+      {icons: 'fab fa-snapchat-ghost', link: 'https://www.snapchat.com/add/blavityfam'},
+    ],
+    footermenu: [
+      {name: 'WRITE A STORY', link: '/post'},
+      {name: 'NEWS', link: '/categories/news'},
+      {name: 'TECH', link: '/categories/technology'},
+      {name: 'VIDEOS', link: '/videos'},
+      {name: 'OPINION', link: '/categories/opinion'},
+      {name: 'MUSIC', link: '/categories/music'},
+      {name: 'About', link: '/categories/about'},
+      {name: 'Team', link: '/team'},
+      {name: 'Advertise', link: 'https://partnerships.blavity.com/'},
+      {name: 'Privacy', link: '/privacy'},
+      {name: 'TOS', link: '/terms-of-service'},
+      {name: 'Jobs', link: '/jobs'},
+      {name: 'Afrotech', link: 'https://afrotech.com/'},
+      {name: 'Press', link: '/press'},
+    ],
+    footerContent: ' is a community of the most exceptional multi-cultural creators and influencers in the world. We partner with diverse content creators and influencers to help them reach a wider audience, amplify their message, and fund their hustles. We believe that the world shifts according to the way people see it. If you change the way people view the world, you can transform it'
+})
 }
