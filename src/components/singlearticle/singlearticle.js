d730a1d3507e3a44c1b12ecea48624e0e9448b9b
ldeveloper7 <lanetteam.mayur@gmail.com>
2018-08-11 16:17:58 +0530

    replaceLegacyBlavityImage & removeSortCode function added

--- src/components/singlearticle/singlearticle.js
+++ src/components/singlearticle/singlearticle.js
@@ -27,6 +27,8 @@ export default {
         .then((res) => {
           if (res.data) {
             this.singlearticle = res.data
+            this.singlearticle.body = this.replaceLegacyBlavityImage(this.singlearticle.body)
+            this.singlearticle.body = this.removeSortCode(this.singlearticle.body)
           } else {
             this.$router.push('/404')
           }
@@ -34,6 +36,33 @@ export default {
         .catch(e => {
           console.log(e)
         })
+    },
+    replaceLegacyBlavityImage: function (content) {
+      content = content && content || ''
+      var sortcode = content.match(/src="http:\/\/blavity.com\/wp-content\/uploads/g);
+      var mainresponce = content
+      if (sortcode !== null) {
+        sortcode.forEach(function (element) {
+          var el = element
+          element = element.replace(/src="http:\/\/blavity.com\/wp-content\/uploads/g, 'src="http://legacy.blavity.com/wp-content/uploads')
+          mainresponce = mainresponce.replace(el, element);
+        })
+      }
+      return mainresponce;
+    },
+    removeSortCode: function (content) {
+      content = content && content || '';
+      var sortcode = content.match(/((\[)+[\w|\s|\d|\=|\;|\&|\"]+(\]))+[\w|\s|\d|\=|\;|\&|\:|\.|\-|\"|\'|\/|\<|\>|\"|\0|\?|\*|\_|\+|\,|\é]+((\[\/+([a-z,A-Z,0-9])+\]))+/g);
+      var mainresponce = content;
+      if (sortcode !== null) {
+        sortcode.forEach(function (element) {
+          var el = element;
+          element = element.replace(/((\[)+[\w|\s|\d|\=|\;|\&|\"]+(\]))+/g, '');
+          element = element.replace(/((\[\/+([a-z,A-Z,0-9])+\]))/g, '');
+          mainresponce = mainresponce.replace(el, element);
+        });
+      }
+      return mainresponce;
     }
   }
 }
