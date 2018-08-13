77c560c95a939a8c99c80de37614c8668299399b
mayurkapadia35 <lanetteam.mayur35@gmail.com>
2018-08-13 18:49:46 +0530

    Apply a filter for displaying tag and category.

--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ import router from './router'
 import 'es6-promise/auto'
 Vue.config.productionTip = false
 
+Vue.filter('capitalize', (value) => {
+  if (!value) return ''
+  value = value.toString()
+  return value.charAt(0).toUpperCase() + value.slice(1)
+})
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
