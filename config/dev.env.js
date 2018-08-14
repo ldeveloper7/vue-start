993f2483fbaf7c236243509f475e0ee6bb64409f
ldeveloper7 <lanetteam.mayur@gmail.com>
2018-08-14 15:13:29 +0530

    changed localapi path

--- config/dev.env.js
+++ config/dev.env.js
@@ -6,6 +6,6 @@ module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   AppName: '"Blavity"',
   LiveAPI: '"https://api.blavity.com/v1/"',
-  LocalAPI: '"http://192.168.200.25:3100/v1/"',
+  LocalAPI: '"http://192.168.200.12:3100/v1/"',
   cloudinaryImageUrl: '"https://res.cloudinary.com/blavity/image/upload/c_crop,g_center,w_auto,q_auto:best,g_south_east,x_0/"',
 })
