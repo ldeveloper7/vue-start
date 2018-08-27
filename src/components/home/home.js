73885c59979fd210c3b4c70a1a57eea431843e8e
ldeveloper7 <lanetteam.mayur@gmail.com>
2018-08-27 13:08:33 +0530

    changed home page with new API

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -41,10 +41,10 @@ export default {
   mounted () {
     let _this = this
     axios.all([
-      axios.get(process.env.LiveAPI + 'articles' + '/' + _this.articleLoadCount + '/' + _this.articleTotalCount).catch(null),
-      axios.get(process.env.LiveAPI + 'articles/type/video/2').catch(null),
-      axios.get(process.env.LiveAPI + 'articles/categories/community-submitted/3').catch(null)
-    ]).then(axios.spread(function (res1, res2, res3, res4) {
+      axios.get(process.env.LiveAPI + 'articles?' + 'featured=true&' + 'count=' + _this.articleLoadCount).catch(null),
+      axios.get(process.env.LiveAPI + 'videos?sort=newer&count=2').catch(null),
+      axios.get(process.env.LiveAPI + 'resources?sort=newer&count=3').catch(null)
+    ]).then(axios.spread(function (res1, res2, res3) {
       _this.articleData = res1.data
       _this.videoData = res2.data
       _this.communityData = res3.data
