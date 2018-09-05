77eff35f3f8bedbf1b60cb18a73d3eaecc98d36e
lDeveloper <lanetteam.mayur@gmail.com>
2018-09-05 14:33:45 +0530

    Revert "changed home page with new API (#45)"
    This reverts commit c0a0062d5d6d0f8de70afa89a75ea19dc3cc98df.

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,8 +1,4 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"',
-  AppName: '"Blavity"',
-  LiveAPI: '"https://api.blavity.io/"',
-  LocalAPI: '"http://192.168.200.12:3100/v1/"',
-  cloudinaryImageUrl: '"https://res.cloudinary.com/blavity/image/upload/c_crop,g_center,w_auto,q_auto:best,g_south_east,x_0/"',
+  NODE_ENV: '"production"'
 }
--- src/components/video/video.js
+++ src/components/video/video.js
@@ -26,21 +26,26 @@ export default {
   },
   methods: {
     loadmore: function () {
-      axios.all([
-        axios.get(process.env.LiveAPI + '/videos?category=founder_interviews&count=' + this.loadCount + '&sort=newer').catch(null),
-        axios.get(process.env.LiveAPI + '/videos?category=startup&count=' + this.loadCount + '&sort=newer').catch(null),
-        axios.get(process.env.LiveAPI + '/videos?category=latest&count=' + this.loadCount + '&sort=newer').catch(null),
-        axios.get(process.env.LiveAPI + '/videos?category=tech_and_culture&count=' + this.loadCount + '&sort=newer').catch(null),
-        axios.get(process.env.LiveAPI + '/videos?category=afrotech_2017&count=' + this.loadCount + '&sort=newer').catch(null)
-      ]).then(axios.spread(function (res1, res2, res3, res4, res5) {
-        this.info = res1.data
-        this.info.push(...res2.data)
-        this.info.push(...res3.data)
-        this.info.push(...res4.data)
-        this.info.push(...res5.data)
-      })).catch(e => {
-        console.log(e)
-      })
+      axios
+        .get(process.env.LiveAPI + 'articles/type/video/' + this.loadCount + '/' + (this.totalcount))
+        .then(response => {
+          if (response.data.length) {
+            if (!this.totalcount) {
+              this.info = (response.data)
+            } else {
+              this.info.push(...response.data)
+            }
+            this.totalcount = this.totalcount + response.data.length
+            if (response.data.length < this.loadCount) {
+              this.loadMoreButton = 0
+            }
+          } else {
+            this.loadMoreButton = 0
+          }
+        })
+        .catch(e => {
+          console.log(e)
+        })
     }
   }
 }
